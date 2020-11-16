efc92ab8ce1493819579d41f2929e96b5ff2504b
4lexandreC <corsinu.alexandre@gmail.com>
2020-11-16 11:36:46 -0300

    localhost blockchain test, dir renamed

---
+++ _ps6files/blockchain/pow.py
@@ -0,0 +1,9 @@
+from hashlib import sha256
+#Let’s decide that the hash of some integer x multiplied by another y must end in 0
+x = 5
+y = 0  # We don't know what y should be yet...
+while sha256(f'{x*y}'.encode()).hexdigest()[-1] != "0":
+    y += 1
+print(f'The solution is y = {y}')
+#hash(5 * 21) = 1253e9373e...5e3600155e860
+
